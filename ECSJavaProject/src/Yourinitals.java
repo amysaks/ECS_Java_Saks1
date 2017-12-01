529bd013efef5590fa41bf668b81fbdbf2752142
amysaks <SaksA9371@sduhsd.lan>
2017-12-01 10:46:24 -0800

    1 & 2
    almost finished with #2

---
+++ ECSJavaProject/src/Yourinitals.java
@@ -0,0 +1,29 @@
+
+public class Yourinitals 
+{
+	
+		
+		
+		public static void main(String[] args)
+		{
+			System.out.println("                                  A                                                                                        SS SSSSSSSS S                        ");	
+			System.out.println("                              A     A                                                                                  S                          S                       ");	
+			System.out.println("                           A            A                                                                           S                               S                      ");	
+			System.out.println("                        A                  A                                                                    S                                     S          ");	
+			System.out.println("                     A                        A                                                                 S                                                            ");	
+			System.out.println("               	  AAAAAAAAAAAA A                                                                   S                                                       ");	
+			System.out.println("                A                                   A                                                                      S                                            ");	
+			System.out.println("             A                                         A                                                                         S                                   ");	
+			System.out.println("        	   A                                             A                                                                              S                             ");	
+			System.out.println("                                                                                                                                                 S                       ");	
+			System.out.println("                                                                                                                                                       S                 ");	
+			System.out.println("                                                                                                             S                                       S                 ");	
+			System.out.println("                                                                                                                S                                  S                    ");	
+			System.out.println("                                                                                                                    S                          S                          ");	
+			System.out.println("                                                                                                                       S                   S                              ");	
+			System.out.println("                                                                                                                        SSSSSSSSS S                                 ");	
+			
+			
+		}
+		
+	}
---
+++ Variables/src/UsingVariables.java
@@ -0,0 +1,22 @@
+
+public class UsingVariables
+{
+
+	public static void main(String[] args)
+	{
+		int Room;
+		double E;
+		String CS;
+	
+		Room= 113;
+		E= 2.71828;
+		CS= "Computer Science";
+	
+		
+		System.out.println("This room is # " + Room + ". " );	
+		System.out.println("e is close to " + E + ". " );	
+		System.out.println("I'm learning about " + CS + ". " );	
+		
+	}
+
+}
