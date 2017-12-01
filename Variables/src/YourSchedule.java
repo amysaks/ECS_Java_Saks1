43cdcb910e418b235de557df12da297a9075d1e4
amysaks <SaksA9371@sduhsd.lan>
2017-12-01 11:04:29 -0800

    1 & 2 Completed
    Completed

--- Variables/src/YourSchedule.java
+++ Variables/src/YourSchedule.java
@@ -1,4 +1,24 @@
 
-public class YourSchedule {
+public class YourSchedule
+{
+	public static void main(String[] args)
+	{
+		String APUSH , VO, ECS, M, ASL  , P, Phy, D;
+		
+		APUSH = "AP US History ";
+		VO = "Van Over";
+		ECS = "Exploring Computer Science";
+		M = "Mauro";
+		ASL= "American Sign Language";
+		P= "Parker";
+		Phy="AP Physics 1";
+		D ="Danssaert";
+		
+		System.out.println("Per 1| " + APUSH +        "|"      +    VO +      "|"      );	
+		System.out.println("Per 2|" + ECS +               "|"        +   M +       "|"        );	
+		System.out.println("Per 3| " + ASL +              "|"          + P +        "|"         );	
+		System.out.println("Per 4|" + Phy +               "|"          + D +        "|"           );	
+		
+	}
 
 }
